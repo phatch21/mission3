74f3b2197ed196dfcdda9a049e2e4f83ea5ac475
phatch21 <phatch21@byu.edu>
2025-01-22 14:07:56 -0700

    started creation of methods

--- FoodItem.cs
+++ FoodItem.cs
@@ -8,5 +8,27 @@ namespace mission3
 {
     internal class FoodItem
     {
+        int[] foodItems = [];
+
+
+        public void FoodItems(int item)
+        {  
+            
+            if (item == 1)
+            {
+                
+                Console.WriteLine("(Please enter the name of food item you would like to add:)");
+                string itemName = Console.ReadLine();
+
+                Console.WriteLine("(Please enter the category of food item:)");
+                string itemCategory = Console.ReadLine();
+
+                Console.WriteLine("(Please enter the quantity of this food item:)");
+                int numAdd = int.Parse(Console.ReadLine());
+            }
+            
+            
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using mission3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the Food Bank Inventory System!");
+        Console.Write("How can we help you today? ");
+        Console.WriteLine("(Please enter the number of the desired item from the menu below:)");
+        Console.WriteLine("1: Add Food Items");
+        Console.WriteLine("2: Delete Food Items");
+        Console.WriteLine("3: Print List of Current Food Items");
+        Console.WriteLine("4: Exit the Program");
+
+
+    }
+
+}
