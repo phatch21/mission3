2c0e65846666c8622fd53119b0cc984ce74142fd
phatch21 <phatch21@byu.edu>
2025-01-22 14:14:17 -0700

    more edits

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ internal class Program
         Console.WriteLine("Welcome to the Food Bank Inventory System!");
         Console.Write("How can we help you today? ");
         Console.WriteLine("(Please enter the number of the desired item from the menu below:)");
+
         Console.WriteLine("1: Add Food Items");
         Console.WriteLine("2: Delete Food Items");
         Console.WriteLine("3: Print List of Current Food Items");
         Console.WriteLine("4: Exit the Program");
 
+        int selection = int.Parse(Console.ReadLine());
+
+        FoodItem item = new FoodItem();
+        item.FoodItems(selection);
 
     }
 
